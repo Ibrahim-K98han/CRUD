c5ad6a006f7fcc117be530ddfcf6c318f07c0cf8
Ibrahim-K98han <aitvetibrahim@gmail.com>
2023-04-01 12:12:41 +0600

    day work done

--- lib/listView.dart
+++ lib/listView.dart
@@ -13,48 +13,59 @@ class ListViewData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<ListViewDataProvider>(
-      builder: (context, provider, _){
-        return Scaffold(appBar: AppBar(title: Text('Home'),),
-          body: Column(
-            children: [
-              SizedBox(
-                height: 300,
-                child: ListView.builder(
-                  itemCount: provider.info.length,
-                  itemBuilder: (context, index){
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          color: Colors.green,
-                        ),
-                        height: 100,
-                        child: Center(
-                          child: ListTile(
-                            title: Text(provider.info[index].name,style: TextStyle(color: Colors.white),),
-                            //subtitle: Text(provider.info[index].designation,style: TextStyle(color: Colors.white),),
-                            onTap: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (BuildContext)=>ListViewDetailsPage(),settings: RouteSettings(arguments: provider.info[index],
-                  ),));
-                            },
+      builder: (context, provider, _) {
+        return Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+            ),
+            body: Column(
+              children: [
+                SizedBox(
+                  height: 300,
+                  child: ListView.builder(
+                    itemCount: provider.info.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.green,
+                          ),
+                          height: 100,
+                          child: Center(
+                            child: ListTile(
+                              title: Text(
+                                provider.info[index].name,
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              //subtitle: Text(provider.info[index].designation,style: TextStyle(color: Colors.white),),
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (BuildContext) =>
+                                          ListViewDetailsPage(),
+                                      settings: RouteSettings(
+                                        arguments: provider.info[index],
+                                      ),
+                                    ));
+                              },
                             ),
+                          ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
                 ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(minimumSize: Size(100, 40)),
-                onPressed: (){
-                  //Navigator.push(context, MaterialPageRoute(builder: (BuildContext)=>ListViewDetailsPage(info: provider.info,),));
-                }, 
-                child: Text('Click')
-                )
-            ],
-          )
-       );
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(minimumSize: Size(100, 40)),
+                    onPressed: () {
+                      //Navigator.push(context, MaterialPageRoute(builder: (BuildContext)=>ListViewDetailsPage(info: provider.info,),));
+                    },
+                    child: Text('Click'))
+              ],
+            ));
       },
     );
   }
